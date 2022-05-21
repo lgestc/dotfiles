cc3283c1df7323292fdd371e195cb7ad28d58ec1
lgmys <lgmys@pm.me>
2022-05-21 12:09:07 +0200

    add zsh prefs and aliases

---
+++ dot_config/aliases.sh
@@ -0,0 +1,15 @@
+# Append this to .zshrc to source aliases
+# source $HOME/.config/aliases.sh
+
+alias cdp='cd ~/projects'
+alias cdpf='cd ~/projects/frontend'
+alias cdc='cd ~/.config'
+alias nvimconf='cdc && cd ./nvim && nvim init.lua'
+
+# show all git changes (staged and not)
+alias gdh="git diff HEAD"
+alias vim="nvim"
+alias vi="nvim"
+alias cal="cal -m -3"
+
+alias nnn="nnn -dex"
---
+++ dot_config/executable_zsh-preferences.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+ZSH_THEME="minimal"
+plugins=(git nvm npm last-working-dir rust)
+
+source $ZSH/oh-my-zsh.sh
+source $HOME/.config/aliases.sh
