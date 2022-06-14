d93fd0af52c51fae5d789ed8f2e13bc6a61488a1
lgmys <lgmys@pm.me>
2022-06-14 09:59:56 +0200

    update aliases and add improvements to up script

--- dot_config/aliases.sh
+++ dot_config/aliases.sh
@@ -13,11 +13,13 @@ alias cdpk='cd ~/projects/elastic/kibana'
 alias cdpt='cd ~/projects/elastic/kibana/x-pack/plugins/threat_intelligence'
 
 # devservers
-alias kibanadev='cdpk && concurrently -n "kibana,search" "yarn start" "yarn es snapshot -E path.data=/home/luke/projects/elastic/esdata"'
+alias kibanadev='cdpk && concurrently -n "kibana,search" "yarn start" "yarn es snapshot --license trial -E path.data=/home/luke/projects/elastic/esdata"'
 alias kbd='kibanadev'
 
 # show all git changes (staged and not)
 alias gdh="git diff HEAD"
+# show conflicts only
+alias gsc="git diff --name-only --diff-filter=U --relative"
 
 # general programs
 alias vim="nvim"
@@ -26,4 +28,4 @@ alias cal="cal -m -3"
 alias wttr="curl wttr.in"
 
 alias alacritty="~/.config/scripts/alacritty"
-alias ls='nnn -de'
+alias ls='n'
